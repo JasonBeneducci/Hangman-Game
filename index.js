a72efe9542283fff8ddb37b79ee124dc378dc3e9
github username <github email address>
2019-10-01 17:36:31 -0400

    Changed the look of buttons to be square and added black categories box

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 Array.prototype.random = function () {
-    return this[Math.floor((Math.random() * this.length))]
+  return this[Math.floor((Math.random() * this.length))]
 }
 const topContainer = document.querySelector("#hangman-image-container")
 const categoryContainer = document.querySelector("#category-container")
@@ -7,75 +7,78 @@ const phraseContainer = document.querySelector("#phrase-container")
 const lettersContainer = document.querySelector("#letter-possibilities-container")
 const phraseAnswerBlocks = document.getElementById("phrase-answer_blocks")
 let gameHash = {}
+let pressedLetter
 let filterCategory = ""
 let filteredQuotes = ""
 let allQuotes = []
+let corrospondingLetter
 let allAnswerBlocksDiv
-const lettersArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+const lettersArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-document.addEventListener("DOMContentLoaded", function (){
-  
-  
-const hangmanImageContainer = document.querySelector("#hangman-image-container")
-const categories = ["Movie Quotes", "Professional Sport Teams", "Song Lyrics"]
+document.addEventListener("DOMContentLoaded", function () {
 
 
-
-createAllLetterBlocks() 
-    
-        
+  const hangmanImageContainer = document.querySelector("#hangman-image-container")
+  const categories = ["Movie Quotes", "Professional Sport Teams", "Song Lyrics"]
 
 
-    // Creates the "New Game" button and Categories dropdown
+
+  createAllLetterBlocks()
 
 
-    categoryContainer.insertAdjacentHTML("beforeend", `
+
+
+  // Creates the "New Game" button and Categories dropdown
+
+
+  categoryContainer.insertAdjacentHTML("beforeend", `
     <div></div>
-    <h1>Select a category</h1>
+    <h1 style="color:white;">Select a category</h1>
       <form id="gameCategoryForm" action="">
         <select id="setCategory">
         </select>
-        <input type="button" value="category" onclick="filterAllChooseRandom()" />
+        <input type="button" value="New Game" onclick="filterAllChooseRandom()" />
        </form>
     `)
-    let dropdown = document.getElementById("setCategory")
-    categories.forEach( function(cat) {
-      dropdown.insertAdjacentHTML("beforeend", `
+  let dropdown = document.getElementById("setCategory")
+  categories.forEach(function (cat) {
+    dropdown.insertAdjacentHTML("beforeend", `
          <option  value="${cat}">${cat}</option>
-      `)}
-    )
+      `)
+  }
+  )
 
-  
 
-    fetch("http://localhost:3000/api/v1/quotes")
+
+  fetch("http://localhost:3000/api/v1/quotes")
     .then(response => response.json())
     .then(data => {
-        allQuotes = data
+      allQuotes = data
     })
 })
 
-    //Builds all letter blocks upon page load
-    createAllLetterBlocks()
+//Builds all letter blocks upon page load
+createAllLetterBlocks()
 
 /// -------------- Support Functions -----------
 
-  // Creates all the letter blocks
-  function createAllLetterBlocks() {
-    lettersContainer.innerHTML = ""
-    lettersArr.forEach( function(let) {
-      lettersContainer.insertAdjacentHTML('beforeend',`<div class="col">
-          <button id="letter-possibility-button" data-id="${let}" type="button" class="btn btn-warning">${let}</button></div>
+// Creates all the letter blocks
+function createAllLetterBlocks() {
+  lettersContainer.innerHTML = ""
+  lettersArr.forEach(function (let) {
+    lettersContainer.insertAdjacentHTML('beforeend', `<div class="col">
+          <button id="letter-possibility-button" data-id="${let}" type="button">${let}</button></div>
       `) // ends lettersContainer.insertAdjacentHTML(
-    })// Ends lettersArr.forEach loop
-  } //  Ends createAllLetterBlocks() function
+  })// Ends lettersArr.forEach loop
+} //  Ends createAllLetterBlocks() function
 
 
 function hashOfLettersAndIndexes(phrase) {
   let letterIndex = 0
   let letter = ""
   let outputHash = {}
-  let splitPhrase = phrase.toUpperCase().replace(/ /g,"_").split("")
-  for ( letterindex = 0; letterindex < splitPhrase.length; letterindex++) {
+  let splitPhrase = phrase.toUpperCase().replace(/ /g, "_").split("")
+  for (letterindex = 0; letterindex < splitPhrase.length; letterindex++) {
     if (/[A-Z]/i.test(splitPhrase[letterindex])) {
 
       if (!!outputHash[splitPhrase[letterindex]]) {
@@ -93,23 +96,25 @@ function hashOfLettersAndIndexes(phrase) {
 
 function buildEmptyLetterBlocks(phrase) {
   phraseAnswerBlocks.innerHTML = ""
-  splitPhrase = phrase.toUpperCase().replace(/ /g,"_").split("")
+  splitPhrase = phrase.toUpperCase().replace(/ /g, "_").split("")
   blockCount = phrase.length
-  for (b = 0; b < blockCount; b++ )    {
-    if (splitPhrase[b] === "_") { 
-      phraseAnswerBlocks.insertAdjacentHTML("beforeend", `<button class="empty-block-space" data-answer-index=${b}></button>` )
+  for (b = 0; b < blockCount; b++) {
+    if (splitPhrase[b] === "_") {
       phraseAnswerBlocks.insertAdjacentHTML("beforeend", `<button class="empty-block-space" data-answer-index=${b}></button>`)
-    } else if (/[A-Z]/i.test(splitPhrase[b])){
-      phraseAnswerBlocks.insertAdjacentHTML("beforeend", 
+      phraseAnswerBlocks.insertAdjacentHTML("beforeend", `<button class="empty-block-space" data-answer-index=${b}></button>`)
+    } else if (/[A-Z]/i.test(splitPhrase[b])) {
+      phraseAnswerBlocks.insertAdjacentHTML("beforeend",
         `<button class="empty-block" data-answer-index=${b}></button>`
-    )} else {
+      )
+    } else {
       phraseAnswerBlocks.insertAdjacentHTML("beforeend",
         `<span class="punctuation-span">${splitPhrase[b]}</span>`
-    )}
+      )
+    }
 
     // Ends the phraseAnswerBlocks.insertAdjacentHTML
 
-    }
+  }
 } // ends buildEmptyLetterBlocks Funciton
 
 
@@ -119,7 +124,7 @@ function buildEmptyLetterBlocks(phrase) {
 function filterAllChooseRandom() {
   createAllLetterBlocks()
   filterCategory = document.getElementById("setCategory").value
-  filteredQuotes = allQuotes.filter( function(q) { return q.category == filterCategory })
+  filteredQuotes = allQuotes.filter(function (q) { return q.category == filterCategory })
 
   let phrase = filteredQuotes.random().quote
   phraseContainer.innerHTML = ""
@@ -129,18 +134,19 @@ function filterAllChooseRandom() {
   // 1- upcases all the letters and swaps *spaces* for underscores
   // 2- Splits the string into an Array
   // 3- creates a new Hash (named gameHash) with k:v of UniqueLetter:[array of index
-       // numbers for the letter]
+  // numbers for the letter]
   gameHash = hashOfLettersAndIndexes(phrase)
   //This method creates empty blocks for each of the letters in the phrase
   buildEmptyLetterBlocks(phrase)
-    
+
 } //
 
-  // function wrongLetterPick() {
+function wrongLetterPick() {
+  console.log("you picked the wrong letter")
   //   count--
 
   //   insertAdjacentHTML('afterbegin', `<img src="${count}.jpg" id="hangman-image">`)
-    
+
 
 
   //   wrontCount.goup
@@ -148,40 +154,51 @@ function filterAllChooseRandom() {
   //   picture.change
 
   //   // console.log("Wrong letter fucntion running")
-  // }  // Ends Wrong Letter
+}  // Ends Wrong Letter
 
-  function actOnPlayedLetter(letter) {
-    // debugger
-    if (!!gameHash[letter]) {
-      indexesOfPickedLetterArr = gameHash[letter]
-      delete gameHash[letter]
-      allAnswerBlocksDiv = document.getElementById("phrase-answer_blocks")
-      indexesOfPickedLetterArr.forEach( function(index) {
+function actOnPlayedLetter(letter) {
+  // debugger
+  if (!!gameHash[letter]) {
+    indexesOfPickedLetterArr = gameHash[letter]
+    delete gameHash[letter]
+    allAnswerBlocksDiv = document.getElementById("phrase-answer_blocks")
+    indexesOfPickedLetterArr.forEach(function (index) {
       allAnswerBlocksDiv.querySelector(`[data-answer-index = "${index}"]`).innerHTML = letter
       if (Object.keys(gameHash).length == 0) {
-        console.log("YOU WIN")
+        phraseContainer.insertAdjacentHTML('afterbegin', "<p>YOU WIN!!!!!!!!!!!!</p>")
       } // ends the if statement to see if the player won the game
-      }) //Ends indexesOfPickedLetterArr.forEach Loop
-    } else {
-      wrongLetterPick() 
-    }
+    }) //Ends indexesOfPickedLetterArr.forEach Loop
+  } else {
+    wrongLetterPick()
+  }
 
-  } // ends actOnPlayedLetter function
+} // ends actOnPlayedLetter function
 
 
 
 //  ---------- EVENT LISTENERS ------------
 lettersContainer.addEventListener("click", function (e) {
   console.dir(e.target)
-    // add conditional logic here to check if the event target is a letter contained in the current phrase
-        // if it is ...... we want to turn that button green and render that letter into the phraseContainer
-    if (e.target.tagName === "BUTTON") {
-        let targetDiv = e.target.parentElement
-        e.target.remove()
-        targetDiv.insertAdjacentHTML('beforeend', `<button class="successful-letter-possibility" data-id="${e.target.dataset.id}" type="button">${e.target.dataset.id}</button>`)
-        
-        actOnPlayedLetter(e.target.dataset.id)
+  // add conditional logic here to check if the event target is a letter contained in the current phrase
+  // if it is ...... we want to turn that button green and render that letter into the phraseContainer
+  if (e.target.tagName === "BUTTON") {
+    let targetDiv = e.target.parentElement
+    e.target.remove()
+    targetDiv.insertAdjacentHTML('beforeend', `<button id="successful-letter-possibility" data-id="${e.target.dataset.id}" type="button">${e.target.dataset.id}</button>`)
+    actOnPlayedLetter(e.target.dataset.id)
+  } // else if (e.target.tagName === "BUTTON")
+}) // emnds CLICK ON LETTER event listener
 
-    } 
-    // else if (e.target.tagName === "BUTTON")
-})
+document.addEventListener("keydown", function (keypress) {
+  if (keypress.keyCode >= 65 && keypress.keyCode <= 90) {
+    console.log(keypress.code.slice(-1))
+    pressedLetter = keypress.code.slice(-1)
+    corrospondingLetter = lettersContainer.querySelector(`[data-id='${pressedLetter}']`)
+    let targetDiv = corrospondingLetter.parentElement
+    corrospondingLetter.remove()
+    targetDiv.insertAdjacentHTML('beforeend', `<button id="successful-letter-possibility" data-id="${pressedLetter}" type="button">${pressedLetter}</button>`)
+    actOnPlayedLetter(pressedLetter)
+
+  }
+  // do something
+}) // ends KEYPRESS Event Listener
